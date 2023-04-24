d5f4bdd71c554582aa2e58700776b096c5faa794
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2023-04-24 15:20:50 +0200

    Update TrainSystem.java

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.system;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.controller.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
@@ -7,11 +10,27 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
+public class RunTrain extends TimerTask{
+	public TrainController controller;
+	public void run(){
+		if(controller == null) throw new Exception("Controller can't be null");
+		controller.followSpeed();
+	}
+}
+
 public class TrainSystem {
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+	private Timer t = new Timer();
+	private RunTrain task;
+
+	TrainSystem(){
+		task = new RunTrain();
+		task.controller = this.controller;
+		t.schedule(task, 1000);
+	}
 
 	public TrainController getController() {
 		return controller;
